537861923c56d5f5ade28fad67a5d83433409cc9
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2026-05-01 17:19:44 -0700

    feat: Update CRDs (#40)

---
+++ Generated/KubernetesCRDModelGen.SourceGenerator/KubernetesCRDModelGen.SourceGenerator.SourceGenerator/v1alpha1.gkehubnamespaces.gkehub.cnrm.cloud.google.com.g.cs
@@ -0,0 +1,177 @@
+﻿#nullable enable
+using k8s;
+using k8s.Models;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+
+namespace KubernetesCRDModelGen.Models.gkehub.cnrm.cloud.google.com;
+/// <summary>GKEHubNamespace is the Schema for the gkehub API</summary>
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+[KubernetesEntity(Group = KubeGroup, Kind = KubeKind, ApiVersion = KubeApiVersion, PluralName = KubePluralName)]
+public partial class V1alpha1GKEHubNamespaceList : IKubernetesObject<V1ListMeta>, IItems<V1alpha1GKEHubNamespace>
+{
+    public const string KubeApiVersion = "v1alpha1";
+    public const string KubeKind = "GKEHubNamespaceList";
+    public const string KubeGroup = "gkehub.cnrm.cloud.google.com";
+    public const string KubePluralName = "gkehubnamespaces";
+    /// <summary>APIVersion defines the versioned schema of this representation of an object. Servers should convert recognized schemas to the latest internal value, and may reject unrecognized values. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources</summary>
+    [JsonPropertyName("apiVersion")]
+    public string ApiVersion { get; set; } = "gkehub.cnrm.cloud.google.com/v1alpha1";
+
+    /// <summary>Kind is a string value representing the REST resource this object represents. Servers may infer this from the endpoint the client submits requests to. Cannot be updated. In CamelCase. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds</summary>
+    [JsonPropertyName("kind")]
+    public string Kind { get; set; } = "GKEHubNamespaceList";
+
+    /// <summary>ListMeta describes metadata that synthetic resources must have, including lists and various status objects. A resource may have only one of {ObjectMeta, ListMeta}.</summary>
+    [JsonPropertyName("metadata")]
+    public V1ListMeta? Metadata { get; set; }
+
+    /// <summary>List of V1alpha1GKEHubNamespace objects.</summary>
+    [JsonPropertyName("items")]
+    public required IList<V1alpha1GKEHubNamespace> Items { get; set; }
+}
+
+/// <summary>Immutable. The scope that this namespace belongs to.</summary>
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+public partial class V1alpha1GKEHubNamespaceSpecScopeRef
+{
+    /// <summary>The name of the scope. Allowed value: The Google Cloud resource name of a `GKEHubScope` resource (format: `projects/{{project}}/locations/{{location}}/scopes/{{name}}`).</summary>
+    [JsonPropertyName("external")]
+    public string? External { get; set; }
+
+    /// <summary>Name of the referent. More info: https://kubernetes.io/docs/concepts/overview/working-with-objects/names/#names</summary>
+    [JsonPropertyName("name")]
+    public string? Name { get; set; }
+
+    /// <summary>Namespace of the referent. More info: https://kubernetes.io/docs/concepts/overview/working-with-objects/namespaces/</summary>
+    [JsonPropertyName("namespace")]
+    public string? Namespace { get; set; }
+}
+
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+public partial class V1alpha1GKEHubNamespaceSpec
+{
+    /// <summary>Optional. Labels for this Namespace.</summary>
+    [JsonPropertyName("labels")]
+    public IDictionary<string, string>? Labels { get; set; }
+
+    /// <summary>Immutable. The namespaceID of the resource.</summary>
+    [JsonPropertyName("namespaceID")]
+    public required string NamespaceID { get; set; }
+
+    /// <summary>Optional. Namespace-level cluster namespace labels. These labels are applied to the related namespace of the member clusters bound to the parent Scope. Scope-level labels (namespace_labels in the Fleet Scope resource) take precedence over Namespace-level labels if they share a key. Keys and values must be Kubernetes-conformant.</summary>
+    [JsonPropertyName("namespaceLabels")]
+    public IDictionary<string, string>? NamespaceLabels { get; set; }
+
+    /// <summary>Immutable. The scope that this namespace belongs to.</summary>
+    [JsonPropertyName("scopeRef")]
+    public required V1alpha1GKEHubNamespaceSpecScopeRef ScopeRef { get; set; }
+}
+
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+public partial class V1alpha1GKEHubNamespaceStatusConditions
+{
+    /// <summary>Last time the condition transitioned from one status to another.</summary>
+    [JsonPropertyName("lastTransitionTime")]
+    public string? LastTransitionTime { get; set; }
+
+    /// <summary>Human-readable message indicating details about last transition.</summary>
+    [JsonPropertyName("message")]
+    public string? Message { get; set; }
+
+    /// <summary>Unique, one-word, CamelCase reason for the condition&apos;s last transition.</summary>
+    [JsonPropertyName("reason")]
+    public string? Reason { get; set; }
+
+    /// <summary>Status is the status of the condition. Can be True, False, Unknown.</summary>
+    [JsonPropertyName("status")]
+    public string? Status { get; set; }
+
+    /// <summary>Type is the type of the condition.</summary>
+    [JsonPropertyName("type")]
+    public string? Type { get; set; }
+}
+
+/// <summary>ObservedState is the state of the resource as most recently observed in GCP.</summary>
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+public partial class V1alpha1GKEHubNamespaceStatusObservedState
+{
+    /// <summary>Output only. The time at which this namespace was created.</summary>
+    [JsonPropertyName("createTime")]
+    public string? CreateTime { get; set; }
+
+    /// <summary>Output only. The time at which this namespace was deleted.</summary>
+    [JsonPropertyName("deleteTime")]
+    public string? DeleteTime { get; set; }
+
+    /// <summary>Output only. State of the namespace resource.</summary>
+    [JsonPropertyName("state")]
+    public string? State { get; set; }
+
+    /// <summary>Output only. Google-generated UUID for this resource. This is unique across all namespace resources. If a namespace resource is deleted and another with the same name is created, it will have a different uid.</summary>
+    [JsonPropertyName("uid")]
+    public string? Uid { get; set; }
+
+    /// <summary>Output only. The time at which this namespace was last updated.</summary>
+    [JsonPropertyName("updateTime")]
+    public string? UpdateTime { get; set; }
+}
+
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+public partial class V1alpha1GKEHubNamespaceStatus
+{
+    /// <summary>Conditions represent the latest available observations of the GKEHubNamespace&apos;s current state.</summary>
+    [JsonPropertyName("conditions")]
+    public IList<V1alpha1GKEHubNamespaceStatusConditions>? Conditions { get; set; }
+
+    /// <summary>A unique specifier for the GKEHubNamespace resource in GCP.</summary>
+    [JsonPropertyName("externalRef")]
+    public string? ExternalRef { get; set; }
+
+    /// <summary>ObservedGeneration is the generation of the resource that was most recently observed by the Config Connector controller. If this is equal to metadata.generation, then that means that the current reported status reflects the most recent desired state of the resource.</summary>
+    [JsonPropertyName("observedGeneration")]
+    public long? ObservedGeneration { get; set; }
+
+    /// <summary>ObservedState is the state of the resource as most recently observed in GCP.</summary>
+    [JsonPropertyName("observedState")]
+    public V1alpha1GKEHubNamespaceStatusObservedState? ObservedState { get; set; }
+}
+
+/// <summary>GKEHubNamespace is the Schema for the gkehub API</summary>
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+[KubernetesEntity(Group = KubeGroup, Kind = KubeKind, ApiVersion = KubeApiVersion, PluralName = KubePluralName)]
+public partial class V1alpha1GKEHubNamespace : IKubernetesObject<V1ObjectMeta>, ISpec<V1alpha1GKEHubNamespaceSpec>, IStatus<V1alpha1GKEHubNamespaceStatus?>
+{
+    public const string KubeApiVersion = "v1alpha1";
+    public const string KubeKind = "GKEHubNamespace";
+    public const string KubeGroup = "gkehub.cnrm.cloud.google.com";
+    public const string KubePluralName = "gkehubnamespaces";
+    /// <summary>APIVersion defines the versioned schema of this representation of an object. Servers should convert recognized schemas to the latest internal value, and may reject unrecognized values. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources</summary>
+    [JsonPropertyName("apiVersion")]
+    public string ApiVersion { get; set; } = "gkehub.cnrm.cloud.google.com/v1alpha1";
+
+    /// <summary>Kind is a string value representing the REST resource this object represents. Servers may infer this from the endpoint the client submits requests to. Cannot be updated. In CamelCase. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds</summary>
+    [JsonPropertyName("kind")]
+    public string Kind { get; set; } = "GKEHubNamespace";
+
+    /// <summary>Standard object&apos;s metadata. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#metadata</summary>
+    [JsonPropertyName("metadata")]
+    public V1ObjectMeta Metadata { get; set; }
+
+    [JsonPropertyName("spec")]
+    public required V1alpha1GKEHubNamespaceSpec Spec { get; set; }
+
+    [JsonPropertyName("status")]
+    public V1alpha1GKEHubNamespaceStatus? Status { get; set; }
+}
