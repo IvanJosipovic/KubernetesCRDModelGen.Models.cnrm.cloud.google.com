b4dd45942f42fd53b5d7a234d1708aa02c013081
IvanJosipovic <9521987+IvanJosipovic@users.noreply.github.com>
2026-05-02 16:13:42 +0000

    feat: Update CRDs

--- Generated/KubernetesCRDModelGen.SourceGenerator/KubernetesCRDModelGen.SourceGenerator.SourceGenerator/v1alpha1.cloudbuildworkerpools.cloudbuild.cnrm.cloud.google.com.g.cs
+++ Generated/KubernetesCRDModelGen.SourceGenerator/KubernetesCRDModelGen.SourceGenerator.SourceGenerator/v1alpha1.cloudbuildworkerpools.cloudbuild.cnrm.cloud.google.com.g.cs
@@ -36,6 +36,7 @@ public partial class V1alpha1CloudBuildWorkerPoolList : IKubernetesObject<V1List
     public required IList<V1alpha1CloudBuildWorkerPool> Items { get; set; }
 }
 
+/// <summary>Immutable. The network definition that the workers are peered to. If this section is left empty, the workers will be peered to `WorkerPool.project_id` on the service producer network.</summary>
 [global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
 [global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
 public partial class V1alpha1CloudBuildWorkerPoolSpecPrivatePoolV1ConfigNetworkConfigPeeredNetworkRef
@@ -53,38 +54,48 @@ public partial class V1alpha1CloudBuildWorkerPoolSpecPrivatePoolV1ConfigNetworkC
     public string? Namespace { get; set; }
 }
 
+/// <summary>Network configuration for the pool.</summary>
 [global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
 [global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
 public partial class V1alpha1CloudBuildWorkerPoolSpecPrivatePoolV1ConfigNetworkConfig
 {
+    /// <summary>Option to configure network egress for the workers.</summary>
     [JsonPropertyName("egressOption")]
     public string? EgressOption { get; set; }
 
+    /// <summary>Immutable. Subnet IP range within the peered network. This is specified in CIDR notation with a slash and the subnet prefix size. You can optionally specify an IP address before the subnet prefix value. e.g. `192.168.0.0/29` would specify an IP range starting at 192.168.0.0 with a prefix size of 29 bits. `/16` would specify a prefix size of 16 bits, with an automatically determined IP within the peered VPC. If unspecified, a value of `/24` will be used.</summary>
     [JsonPropertyName("peeredNetworkIPRange")]
     public string? PeeredNetworkIPRange { get; set; }
 
+    /// <summary>Immutable. The network definition that the workers are peered to. If this section is left empty, the workers will be peered to `WorkerPool.project_id` on the service producer network.</summary>
     [JsonPropertyName("peeredNetworkRef")]
-    public required V1alpha1CloudBuildWorkerPoolSpecPrivatePoolV1ConfigNetworkConfigPeeredNetworkRef PeeredNetworkRef { get; set; }
+    public V1alpha1CloudBuildWorkerPoolSpecPrivatePoolV1ConfigNetworkConfigPeeredNetworkRef? PeeredNetworkRef { get; set; }
 }
 
+/// <summary>Machine configuration for the workers in the pool.</summary>
 [global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
 [global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
 public partial class V1alpha1CloudBuildWorkerPoolSpecPrivatePoolV1ConfigWorkerConfig
 {
+    /// <summary>Size of the disk attached to the worker, in GB. See [Worker pool config file](https://cloud.google.com/build/docs/private-pools/worker-pool-config-file-schema). Specify a value of up to 2000. If `0` is specified, Cloud Build will use a standard disk size.</summary>
     [JsonPropertyName("diskSizeGb")]
     public long? DiskSizeGb { get; set; }
 
+    /// <summary>Machine type of a worker, such as `e2-medium`. See [Worker pool config file](https://cloud.google.com/build/docs/private-pools/worker-pool-config-file-schema). If left blank, Cloud Build will use a sensible default.</summary>
     [JsonPropertyName("machineType")]
     public string? MachineType { get; set; }
 }
 
+/// <summary>Legacy Private Pool configuration.</summary>
 [global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
 [global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
 public partial class V1alpha1CloudBuildWorkerPoolSpecPrivatePoolV1Config
 {
+    /// <summary>Network configuration for the pool.</summary>
     [JsonPropertyName("networkConfig")]
     public V1alpha1CloudBuildWorkerPoolSpecPrivatePoolV1ConfigNetworkConfig? NetworkConfig { get; set; }
 
+    /// <summary>Machine configuration for the workers in the pool.</summary>
     [JsonPropertyName("workerConfig")]
     public required V1alpha1CloudBuildWorkerPoolSpecPrivatePoolV1ConfigWorkerConfig WorkerConfig { get; set; }
 }
@@ -116,15 +127,14 @@ public partial class V1alpha1CloudBuildWorkerPoolSpecProjectRef
 [global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
 public partial class V1alpha1CloudBuildWorkerPoolSpec
 {
+    /// <summary>A user-specified, human-readable name for the `WorkerPool`. If provided, this value must be 1-63 characters.</summary>
     [JsonPropertyName("displayName")]
     public string? DisplayName { get; set; }
 
     [JsonPropertyName("location")]
     public required string Location { get; set; }
 
-    [JsonPropertyName("name")]
-    public string? Name { get; set; }
-
+    /// <summary>Legacy Private Pool configuration.</summary>
     [JsonPropertyName("privatePoolV1Config")]
     public required V1alpha1CloudBuildWorkerPoolSpecPrivatePoolV1Config PrivatePoolV1Config { get; set; }
 
@@ -132,6 +142,7 @@ public partial class V1alpha1CloudBuildWorkerPoolSpec
     [JsonPropertyName("projectRef")]
     public required V1alpha1CloudBuildWorkerPoolSpecProjectRef ProjectRef { get; set; }
 
+    /// <summary>The GCP resource identifier. If not given, the metadata.name will be used.</summary>
     [JsonPropertyName("resourceID")]
     public string? ResourceID { get; set; }
 }
@@ -161,27 +172,34 @@ public partial class V1alpha1CloudBuildWorkerPoolStatusConditions
     public string? Type { get; set; }
 }
 
+/// <summary>Network configuration for the pool.</summary>
 [global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
 [global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
 public partial class V1alpha1CloudBuildWorkerPoolStatusObservedStateNetworkConfig
 {
+    /// <summary>Option to configure network egress for the workers.</summary>
     [JsonPropertyName("egressOption")]
     public string? EgressOption { get; set; }
 
+    /// <summary>Immutable. The network definition that the workers are peered to. If this section is left empty, the workers will be peered to `WorkerPool.project_id` on the service producer network.</summary>
     [JsonPropertyName("peeredNetwork")]
     public string? PeeredNetwork { get; set; }
 
+    /// <summary>Immutable. Subnet IP range within the peered network. This is specified in CIDR notation with a slash and the subnet prefix size. You can optionally specify an IP address before the subnet prefix value. e.g. `192.168.0.0/29` would specify an IP range starting at 192.168.0.0 with a prefix size of 29 bits. `/16` would specify a prefix size of 16 bits, with an automatically determined IP within the peered VPC. If unspecified, a value of `/24` will be used.</summary>
     [JsonPropertyName("peeredNetworkIPRange")]
     public string? PeeredNetworkIPRange { get; set; }
 }
 
+/// <summary>Machine configuration for the workers in the pool.</summary>
 [global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
 [global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
 public partial class V1alpha1CloudBuildWorkerPoolStatusObservedStateWorkerConfig
 {
+    /// <summary>Size of the disk attached to the worker, in GB. See [Worker pool config file](https://cloud.google.com/build/docs/private-pools/worker-pool-config-file-schema). Specify a value of up to 2000. If `0` is specified, Cloud Build will use a standard disk size.</summary>
     [JsonPropertyName("diskSizeGb")]
     public long? DiskSizeGb { get; set; }
 
+    /// <summary>Machine type of a worker, such as `e2-medium`. See [Worker pool config file](https://cloud.google.com/build/docs/private-pools/worker-pool-config-file-schema). If left blank, Cloud Build will use a sensible default.</summary>
     [JsonPropertyName("machineType")]
     public string? MachineType { get; set; }
 }
@@ -195,6 +213,11 @@ public partial class V1alpha1CloudBuildWorkerPoolStatusObservedState
     [JsonPropertyName("createTime")]
     public DateTime? CreateTime { get; set; }
 
+    /// <summary>The Checksum computed by the server, using weak indicator.</summary>
+    [JsonPropertyName("etag")]
+    public string? Etag { get; set; }
+
+    /// <summary>Network configuration for the pool.</summary>
     [JsonPropertyName("networkConfig")]
     public V1alpha1CloudBuildWorkerPoolStatusObservedStateNetworkConfig? NetworkConfig { get; set; }
 
@@ -202,8 +225,9 @@ public partial class V1alpha1CloudBuildWorkerPoolStatusObservedState
     [JsonPropertyName("updateTime")]
     public DateTime? UpdateTime { get; set; }
 
+    /// <summary>Machine configuration for the workers in the pool.</summary>
     [JsonPropertyName("workerConfig")]
-    public V1alpha1CloudBuildWorkerPoolStatusObservedStateWorkerConfig? WorkerConfig { get; set; }
+    public required V1alpha1CloudBuildWorkerPoolStatusObservedStateWorkerConfig WorkerConfig { get; set; }
 }
 
 /// <summary>CloudBuildWorkerPoolStatus defines the observed state of Instance</summary>
@@ -214,6 +238,10 @@ public partial class V1alpha1CloudBuildWorkerPoolStatus
     /// <summary>Conditions represent the latest available observations of the object&apos;s current state.</summary>
     [JsonPropertyName("conditions")]
     public IList<V1alpha1CloudBuildWorkerPoolStatusConditions>? Conditions { get; set; }
+
+    /// <summary>A unique Config Connector specifier for the resource in GCP.</summary>
+    [JsonPropertyName("externalRef")]
+    public string? ExternalRef { get; set; }
 
     /// <summary>ObservedGeneration is the generation of the resource that was most recently observed by the Config Connector controller. If this is equal to metadata.generation, then that means that the current reported status reflects the most recent desired state of the resource.</summary>
     [JsonPropertyName("observedGeneration")]
