ebfa40c6e830f1a465ed6da56b2318e73d263f68
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2026-06-06 14:23:00 -0700

    feat: Update CRDs (#130)

---
+++ Generated/KubernetesCRDModelGen.SourceGenerator/KubernetesCRDModelGen.SourceGenerator.SourceGenerator/v1alpha1.apihubruntimeprojectattachments.apihub.cnrm.cloud.google.com.g.cs
@@ -0,0 +1,191 @@
+﻿#nullable enable
+using k8s;
+using k8s.Models;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+
+namespace KubernetesCRDModelGen.Models.apihub.cnrm.cloud.google.com;
+/// <summary>APIHubRuntimeProjectAttachment is the Schema for the APIHubRuntimeProjectAttachment API</summary>
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+[KubernetesEntity(Group = KubeGroup, Kind = KubeKind, ApiVersion = KubeApiVersion, PluralName = KubePluralName)]
+public partial class V1alpha1APIHubRuntimeProjectAttachmentList : IKubernetesObject<V1ListMeta>, IItems<V1alpha1APIHubRuntimeProjectAttachment>
+{
+    public const string KubeApiVersion = "v1alpha1";
+    public const string KubeKind = "APIHubRuntimeProjectAttachmentList";
+    public const string KubeGroup = "apihub.cnrm.cloud.google.com";
+    public const string KubePluralName = "apihubruntimeprojectattachments";
+    /// <summary>APIVersion defines the versioned schema of this representation of an object. Servers should convert recognized schemas to the latest internal value, and may reject unrecognized values. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources</summary>
+    [JsonPropertyName("apiVersion")]
+    public string ApiVersion { get; set; } = "apihub.cnrm.cloud.google.com/v1alpha1";
+
+    /// <summary>Kind is a string value representing the REST resource this object represents. Servers may infer this from the endpoint the client submits requests to. Cannot be updated. In CamelCase. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds</summary>
+    [JsonPropertyName("kind")]
+    public string Kind { get; set; } = "APIHubRuntimeProjectAttachmentList";
+
+    /// <summary>ListMeta describes metadata that synthetic resources must have, including lists and various status objects. A resource may have only one of {ObjectMeta, ListMeta}.</summary>
+    [JsonPropertyName("metadata")]
+    public V1ListMeta? Metadata { get; set; }
+
+    /// <summary>List of V1alpha1APIHubRuntimeProjectAttachment objects.</summary>
+    [JsonPropertyName("items")]
+    public required IList<V1alpha1APIHubRuntimeProjectAttachment> Items { get; set; }
+}
+
+/// <summary>The project that this resource belongs to.</summary>
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+public partial class V1alpha1APIHubRuntimeProjectAttachmentSpecProjectRef
+{
+    /// <summary>The `projectID` field of a project, when not managed by Config Connector.</summary>
+    [JsonPropertyName("external")]
+    public string? External { get; set; }
+
+    /// <summary>The kind of the Project resource; optional but must be `Project` if provided.</summary>
+    [JsonPropertyName("kind")]
+    public string? Kind { get; set; }
+
+    /// <summary>The `name` field of a `Project` resource.</summary>
+    [JsonPropertyName("name")]
+    public string? Name { get; set; }
+
+    /// <summary>The `namespace` field of a `Project` resource.</summary>
+    [JsonPropertyName("namespace")]
+    public string? Namespace { get; set; }
+}
+
+/// <summary>Required. Immutable. Google cloud project name in the format: &quot;projects/abc&quot; or &quot;projects/123&quot;. As input, project name with either project id or number are accepted. As output, this field will contain project number.</summary>
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+public partial class V1alpha1APIHubRuntimeProjectAttachmentSpecRuntimeProjectRef
+{
+    /// <summary>The `projectID` field of a project, when not managed by Config Connector.</summary>
+    [JsonPropertyName("external")]
+    public string? External { get; set; }
+
+    /// <summary>The kind of the Project resource; optional but must be `Project` if provided.</summary>
+    [JsonPropertyName("kind")]
+    public string? Kind { get; set; }
+
+    /// <summary>The `name` field of a `Project` resource.</summary>
+    [JsonPropertyName("name")]
+    public string? Name { get; set; }
+
+    /// <summary>The `namespace` field of a `Project` resource.</summary>
+    [JsonPropertyName("namespace")]
+    public string? Namespace { get; set; }
+}
+
+/// <summary>APIHubRuntimeProjectAttachmentSpec defines the desired state of APIHubRuntimeProjectAttachment</summary>
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+public partial class V1alpha1APIHubRuntimeProjectAttachmentSpec
+{
+    /// <summary>The location of this resource.</summary>
+    [JsonPropertyName("location")]
+    public required string Location { get; set; }
+
+    /// <summary>The project that this resource belongs to.</summary>
+    [JsonPropertyName("projectRef")]
+    public required V1alpha1APIHubRuntimeProjectAttachmentSpecProjectRef ProjectRef { get; set; }
+
+    /// <summary>The APIHubRuntimeProjectAttachment name. If not given, the metadata.name will be used.</summary>
+    [JsonPropertyName("resourceID")]
+    public string? ResourceID { get; set; }
+
+    /// <summary>Required. Immutable. Google cloud project name in the format: &quot;projects/abc&quot; or &quot;projects/123&quot;. As input, project name with either project id or number are accepted. As output, this field will contain project number.</summary>
+    [JsonPropertyName("runtimeProjectRef")]
+    public required V1alpha1APIHubRuntimeProjectAttachmentSpecRuntimeProjectRef RuntimeProjectRef { get; set; }
+}
+
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+public partial class V1alpha1APIHubRuntimeProjectAttachmentStatusConditions
+{
+    /// <summary>Last time the condition transitioned from one status to another.</summary>
+    [JsonPropertyName("lastTransitionTime")]
+    public string? LastTransitionTime { get; set; }
+
+    /// <summary>Human-readable message indicating details about last transition.</summary>
+    [JsonPropertyName("message")]
+    public string? Message { get; set; }
+
+    /// <summary>Unique, one-word, CamelCase reason for the condition&apos;s last transition.</summary>
+    [JsonPropertyName("reason")]
+    public string? Reason { get; set; }
+
+    /// <summary>Status is the status of the condition. Can be True, False, Unknown.</summary>
+    [JsonPropertyName("status")]
+    public string? Status { get; set; }
+
+    /// <summary>Type is the type of the condition.</summary>
+    [JsonPropertyName("type")]
+    public string? Type { get; set; }
+}
+
+/// <summary>ObservedState is the state of the resource as most recently observed in GCP.</summary>
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+public partial class V1alpha1APIHubRuntimeProjectAttachmentStatusObservedState
+{
+    /// <summary>Output only. Create time.</summary>
+    [JsonPropertyName("createTime")]
+    public string? CreateTime { get; set; }
+}
+
+/// <summary>APIHubRuntimeProjectAttachmentStatus defines the config connector machine state of APIHubRuntimeProjectAttachment</summary>
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+public partial class V1alpha1APIHubRuntimeProjectAttachmentStatus
+{
+    /// <summary>Conditions represent the latest available observations of the object&apos;s current state.</summary>
+    [JsonPropertyName("conditions")]
+    public IList<V1alpha1APIHubRuntimeProjectAttachmentStatusConditions>? Conditions { get; set; }
+
+    /// <summary>A unique specifier for the APIHubRuntimeProjectAttachment resource in GCP.</summary>
+    [JsonPropertyName("externalRef")]
+    public string? ExternalRef { get; set; }
+
+    /// <summary>ObservedGeneration is the generation of the resource that was most recently observed by the Config Connector controller. If this is equal to metadata.generation, then that means that the current reported status reflects the most recent desired state of the resource.</summary>
+    [JsonPropertyName("observedGeneration")]
+    public long? ObservedGeneration { get; set; }
+
+    /// <summary>ObservedState is the state of the resource as most recently observed in GCP.</summary>
+    [JsonPropertyName("observedState")]
+    public V1alpha1APIHubRuntimeProjectAttachmentStatusObservedState? ObservedState { get; set; }
+}
+
+/// <summary>APIHubRuntimeProjectAttachment is the Schema for the APIHubRuntimeProjectAttachment API</summary>
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+[KubernetesEntity(Group = KubeGroup, Kind = KubeKind, ApiVersion = KubeApiVersion, PluralName = KubePluralName)]
+public partial class V1alpha1APIHubRuntimeProjectAttachment : IKubernetesObject<V1ObjectMeta>, ISpec<V1alpha1APIHubRuntimeProjectAttachmentSpec>, IStatus<V1alpha1APIHubRuntimeProjectAttachmentStatus?>
+{
+    public const string KubeApiVersion = "v1alpha1";
+    public const string KubeKind = "APIHubRuntimeProjectAttachment";
+    public const string KubeGroup = "apihub.cnrm.cloud.google.com";
+    public const string KubePluralName = "apihubruntimeprojectattachments";
+    /// <summary>APIVersion defines the versioned schema of this representation of an object. Servers should convert recognized schemas to the latest internal value, and may reject unrecognized values. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources</summary>
+    [JsonPropertyName("apiVersion")]
+    public string ApiVersion { get; set; } = "apihub.cnrm.cloud.google.com/v1alpha1";
+
+    /// <summary>Kind is a string value representing the REST resource this object represents. Servers may infer this from the endpoint the client submits requests to. Cannot be updated. In CamelCase. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds</summary>
+    [JsonPropertyName("kind")]
+    public string Kind { get; set; } = "APIHubRuntimeProjectAttachment";
+
+    /// <summary>Standard object&apos;s metadata. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#metadata</summary>
+    [JsonPropertyName("metadata")]
+    public V1ObjectMeta Metadata { get; set; }
+
+    /// <summary>APIHubRuntimeProjectAttachmentSpec defines the desired state of APIHubRuntimeProjectAttachment</summary>
+    [JsonPropertyName("spec")]
+    public required V1alpha1APIHubRuntimeProjectAttachmentSpec Spec { get; set; }
+
+    /// <summary>APIHubRuntimeProjectAttachmentStatus defines the config connector machine state of APIHubRuntimeProjectAttachment</summary>
+    [JsonPropertyName("status")]
+    public V1alpha1APIHubRuntimeProjectAttachmentStatus? Status { get; set; }
+}
