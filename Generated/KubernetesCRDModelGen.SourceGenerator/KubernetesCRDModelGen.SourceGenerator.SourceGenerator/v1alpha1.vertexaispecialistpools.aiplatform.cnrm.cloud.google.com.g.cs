258641e6cf7f140f010b2ac5c4979afa1de35250
IvanJosipovic <9521987+IvanJosipovic@users.noreply.github.com>
2026-06-14 03:02:10 +0000

    feat: Update CRDs

---
+++ Generated/KubernetesCRDModelGen.SourceGenerator/KubernetesCRDModelGen.SourceGenerator.SourceGenerator/v1alpha1.vertexaispecialistpools.aiplatform.cnrm.cloud.google.com.g.cs
@@ -0,0 +1,181 @@
+﻿#nullable enable
+using k8s;
+using k8s.Models;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+
+namespace KubernetesCRDModelGen.Models.aiplatform.cnrm.cloud.google.com;
+/// <summary>VertexAISpecialistPool is the Schema for the VertexAISpecialistPool API</summary>
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+[KubernetesEntity(Group = KubeGroup, Kind = KubeKind, ApiVersion = KubeApiVersion, PluralName = KubePluralName)]
+public partial class V1alpha1VertexAISpecialistPoolList : IKubernetesObject<V1ListMeta>, IItems<V1alpha1VertexAISpecialistPool>
+{
+    public const string KubeApiVersion = "v1alpha1";
+    public const string KubeKind = "VertexAISpecialistPoolList";
+    public const string KubeGroup = "aiplatform.cnrm.cloud.google.com";
+    public const string KubePluralName = "vertexaispecialistpools";
+    /// <summary>APIVersion defines the versioned schema of this representation of an object. Servers should convert recognized schemas to the latest internal value, and may reject unrecognized values. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources</summary>
+    [JsonPropertyName("apiVersion")]
+    public string ApiVersion { get; set; } = "aiplatform.cnrm.cloud.google.com/v1alpha1";
+
+    /// <summary>Kind is a string value representing the REST resource this object represents. Servers may infer this from the endpoint the client submits requests to. Cannot be updated. In CamelCase. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds</summary>
+    [JsonPropertyName("kind")]
+    public string Kind { get; set; } = "VertexAISpecialistPoolList";
+
+    /// <summary>ListMeta describes metadata that synthetic resources must have, including lists and various status objects. A resource may have only one of {ObjectMeta, ListMeta}.</summary>
+    [JsonPropertyName("metadata")]
+    public V1ListMeta? Metadata { get; set; }
+
+    /// <summary>List of V1alpha1VertexAISpecialistPool objects.</summary>
+    [JsonPropertyName("items")]
+    public required IList<V1alpha1VertexAISpecialistPool> Items { get; set; }
+}
+
+/// <summary>The project that this resource belongs to.</summary>
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+public partial class V1alpha1VertexAISpecialistPoolSpecProjectRef
+{
+    /// <summary>The `projectID` field of a project, when not managed by Config Connector.</summary>
+    [JsonPropertyName("external")]
+    public string? External { get; set; }
+
+    /// <summary>The kind of the Project resource; optional but must be `Project` if provided.</summary>
+    [JsonPropertyName("kind")]
+    public string? Kind { get; set; }
+
+    /// <summary>The `name` field of a `Project` resource.</summary>
+    [JsonPropertyName("name")]
+    public string? Name { get; set; }
+
+    /// <summary>The `namespace` field of a `Project` resource.</summary>
+    [JsonPropertyName("namespace")]
+    public string? Namespace { get; set; }
+}
+
+/// <summary>VertexAISpecialistPoolSpec defines the desired state of VertexAISpecialistPool</summary>
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+public partial class V1alpha1VertexAISpecialistPoolSpec
+{
+    /// <summary>Required. The user-defined name of the SpecialistPool. The name can be up to 128 characters long and can consist of any UTF-8 characters. This field should be unique on project-level.</summary>
+    [JsonPropertyName("displayName")]
+    public required string DisplayName { get; set; }
+
+    /// <summary>The location of this resource.</summary>
+    [JsonPropertyName("location")]
+    public required string Location { get; set; }
+
+    /// <summary>The project that this resource belongs to.</summary>
+    [JsonPropertyName("projectRef")]
+    public required V1alpha1VertexAISpecialistPoolSpecProjectRef ProjectRef { get; set; }
+
+    /// <summary>The VertexAISpecialistPool name. If not given, the metadata.name will be used.</summary>
+    [JsonPropertyName("resourceID")]
+    public string? ResourceID { get; set; }
+
+    /// <summary>The email addresses of the managers in the SpecialistPool.</summary>
+    [JsonPropertyName("specialistManagerEmails")]
+    public IList<string>? SpecialistManagerEmails { get; set; }
+
+    /// <summary>The email addresses of workers in the SpecialistPool.</summary>
+    [JsonPropertyName("specialistWorkerEmails")]
+    public IList<string>? SpecialistWorkerEmails { get; set; }
+}
+
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+public partial class V1alpha1VertexAISpecialistPoolStatusConditions
+{
+    /// <summary>Last time the condition transitioned from one status to another.</summary>
+    [JsonPropertyName("lastTransitionTime")]
+    public string? LastTransitionTime { get; set; }
+
+    /// <summary>Human-readable message indicating details about last transition.</summary>
+    [JsonPropertyName("message")]
+    public string? Message { get; set; }
+
+    /// <summary>Unique, one-word, CamelCase reason for the condition&apos;s last transition.</summary>
+    [JsonPropertyName("reason")]
+    public string? Reason { get; set; }
+
+    /// <summary>Status is the status of the condition. Can be True, False, Unknown.</summary>
+    [JsonPropertyName("status")]
+    public string? Status { get; set; }
+
+    /// <summary>Type is the type of the condition.</summary>
+    [JsonPropertyName("type")]
+    public string? Type { get; set; }
+}
+
+/// <summary>ObservedState is the state of the resource as most recently observed in GCP.</summary>
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+public partial class V1alpha1VertexAISpecialistPoolStatusObservedState
+{
+    /// <summary>Output only. The resource name of the pending data labeling jobs.</summary>
+    [JsonPropertyName("pendingDataLabelingJobs")]
+    public IList<string>? PendingDataLabelingJobs { get; set; }
+
+    /// <summary>Output only. The number of managers in this SpecialistPool.</summary>
+    [JsonPropertyName("specialistManagersCount")]
+    public int? SpecialistManagersCount { get; set; }
+}
+
+/// <summary>VertexAISpecialistPoolStatus defines the config connector machine state of VertexAISpecialistPool</summary>
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+public partial class V1alpha1VertexAISpecialistPoolStatus
+{
+    /// <summary>Conditions represent the latest available observations of the object&apos;s current state.</summary>
+    [JsonPropertyName("conditions")]
+    public IList<V1alpha1VertexAISpecialistPoolStatusConditions>? Conditions { get; set; }
+
+    /// <summary>A unique specifier for the VertexAISpecialistPool resource in GCP.</summary>
+    [JsonPropertyName("externalRef")]
+    public string? ExternalRef { get; set; }
+
+    /// <summary>ObservedGeneration is the generation of the resource that was most recently observed by the Config Connector controller. If this is equal to metadata.generation, then that means that the current reported status reflects the most recent desired state of the resource.</summary>
+    [JsonPropertyName("observedGeneration")]
+    public long? ObservedGeneration { get; set; }
+
+    /// <summary>ObservedState is the state of the resource as most recently observed in GCP.</summary>
+    [JsonPropertyName("observedState")]
+    public V1alpha1VertexAISpecialistPoolStatusObservedState? ObservedState { get; set; }
+}
+
+/// <summary>VertexAISpecialistPool is the Schema for the VertexAISpecialistPool API</summary>
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+[KubernetesEntity(Group = KubeGroup, Kind = KubeKind, ApiVersion = KubeApiVersion, PluralName = KubePluralName)]
+public partial class V1alpha1VertexAISpecialistPool : IKubernetesObject<V1ObjectMeta>, ISpec<V1alpha1VertexAISpecialistPoolSpec>, IStatus<V1alpha1VertexAISpecialistPoolStatus?>
+{
+    public const string KubeApiVersion = "v1alpha1";
+    public const string KubeKind = "VertexAISpecialistPool";
+    public const string KubeGroup = "aiplatform.cnrm.cloud.google.com";
+    public const string KubePluralName = "vertexaispecialistpools";
+    /// <summary>APIVersion defines the versioned schema of this representation of an object. Servers should convert recognized schemas to the latest internal value, and may reject unrecognized values. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources</summary>
+    [JsonPropertyName("apiVersion")]
+    public string ApiVersion { get; set; } = "aiplatform.cnrm.cloud.google.com/v1alpha1";
+
+    /// <summary>Kind is a string value representing the REST resource this object represents. Servers may infer this from the endpoint the client submits requests to. Cannot be updated. In CamelCase. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds</summary>
+    [JsonPropertyName("kind")]
+    public string Kind { get; set; } = "VertexAISpecialistPool";
+
+    /// <summary>Standard object&apos;s metadata. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#metadata</summary>
+    [JsonPropertyName("metadata")]
+    public V1ObjectMeta Metadata { get; set; }
+
+    /// <summary>VertexAISpecialistPoolSpec defines the desired state of VertexAISpecialistPool</summary>
+    [JsonPropertyName("spec")]
+    public required V1alpha1VertexAISpecialistPoolSpec Spec { get; set; }
+
+    /// <summary>VertexAISpecialistPoolStatus defines the config connector machine state of VertexAISpecialistPool</summary>
+    [JsonPropertyName("status")]
+    public V1alpha1VertexAISpecialistPoolStatus? Status { get; set; }
+}
