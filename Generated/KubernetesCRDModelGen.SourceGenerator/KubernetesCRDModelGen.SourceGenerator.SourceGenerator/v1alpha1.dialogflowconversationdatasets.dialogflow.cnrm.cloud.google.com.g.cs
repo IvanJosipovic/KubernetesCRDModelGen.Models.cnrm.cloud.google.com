8e89679cf4850b43e871902358ef22e204a850d2
IvanJosipovic <9521987+IvanJosipovic@users.noreply.github.com>
2026-06-16 00:33:35 +0000

    feat: Update CRDs

---
+++ Generated/KubernetesCRDModelGen.SourceGenerator/KubernetesCRDModelGen.SourceGenerator.SourceGenerator/v1alpha1.dialogflowconversationdatasets.dialogflow.cnrm.cloud.google.com.g.cs
@@ -0,0 +1,223 @@
+﻿#nullable enable
+using k8s;
+using k8s.Models;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+
+namespace KubernetesCRDModelGen.Models.dialogflow.cnrm.cloud.google.com;
+/// <summary>DialogflowConversationDataset is the Schema for the DialogflowConversationDataset API</summary>
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+[KubernetesEntity(Group = KubeGroup, Kind = KubeKind, ApiVersion = KubeApiVersion, PluralName = KubePluralName)]
+public partial class V1alpha1DialogflowConversationDatasetList : IKubernetesObject<V1ListMeta>, IItems<V1alpha1DialogflowConversationDataset>
+{
+    public const string KubeApiVersion = "v1alpha1";
+    public const string KubeKind = "DialogflowConversationDatasetList";
+    public const string KubeGroup = "dialogflow.cnrm.cloud.google.com";
+    public const string KubePluralName = "dialogflowconversationdatasets";
+    /// <summary>APIVersion defines the versioned schema of this representation of an object. Servers should convert recognized schemas to the latest internal value, and may reject unrecognized values. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources</summary>
+    [JsonPropertyName("apiVersion")]
+    public string ApiVersion { get; set; } = "dialogflow.cnrm.cloud.google.com/v1alpha1";
+
+    /// <summary>Kind is a string value representing the REST resource this object represents. Servers may infer this from the endpoint the client submits requests to. Cannot be updated. In CamelCase. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds</summary>
+    [JsonPropertyName("kind")]
+    public string Kind { get; set; } = "DialogflowConversationDatasetList";
+
+    /// <summary>ListMeta describes metadata that synthetic resources must have, including lists and various status objects. A resource may have only one of {ObjectMeta, ListMeta}.</summary>
+    [JsonPropertyName("metadata")]
+    public V1ListMeta? Metadata { get; set; }
+
+    /// <summary>List of V1alpha1DialogflowConversationDataset objects.</summary>
+    [JsonPropertyName("items")]
+    public required IList<V1alpha1DialogflowConversationDataset> Items { get; set; }
+}
+
+/// <summary>The project that this resource belongs to.</summary>
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+public partial class V1alpha1DialogflowConversationDatasetSpecProjectRef
+{
+    /// <summary>The `projectID` field of a project, when not managed by Config Connector.</summary>
+    [JsonPropertyName("external")]
+    public string? External { get; set; }
+
+    /// <summary>The kind of the Project resource; optional but must be `Project` if provided.</summary>
+    [JsonPropertyName("kind")]
+    public string? Kind { get; set; }
+
+    /// <summary>The `name` field of a `Project` resource.</summary>
+    [JsonPropertyName("name")]
+    public string? Name { get; set; }
+
+    /// <summary>The `namespace` field of a `Project` resource.</summary>
+    [JsonPropertyName("namespace")]
+    public string? Namespace { get; set; }
+}
+
+/// <summary>DialogflowConversationDatasetSpec defines the desired state of DialogflowConversationDataset</summary>
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+public partial class V1alpha1DialogflowConversationDatasetSpec
+{
+    /// <summary>Optional. The description of the dataset. Maximum of 10000 bytes.</summary>
+    [JsonPropertyName("description")]
+    public string? Description { get; set; }
+
+    /// <summary>Required. The display name of the dataset. Maximum of 64 bytes.</summary>
+    [JsonPropertyName("displayName")]
+    public required string DisplayName { get; set; }
+
+    /// <summary>The location of this resource.</summary>
+    [JsonPropertyName("location")]
+    public required string Location { get; set; }
+
+    /// <summary>The project that this resource belongs to.</summary>
+    [JsonPropertyName("projectRef")]
+    public required V1alpha1DialogflowConversationDatasetSpecProjectRef ProjectRef { get; set; }
+
+    /// <summary>The DialogflowConversationDataset name. If not given, the metadata.name will be used.</summary>
+    [JsonPropertyName("resourceID")]
+    public string? ResourceID { get; set; }
+}
+
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+public partial class V1alpha1DialogflowConversationDatasetStatusConditions
+{
+    /// <summary>Last time the condition transitioned from one status to another.</summary>
+    [JsonPropertyName("lastTransitionTime")]
+    public string? LastTransitionTime { get; set; }
+
+    /// <summary>Human-readable message indicating details about last transition.</summary>
+    [JsonPropertyName("message")]
+    public string? Message { get; set; }
+
+    /// <summary>Unique, one-word, CamelCase reason for the condition&apos;s last transition.</summary>
+    [JsonPropertyName("reason")]
+    public string? Reason { get; set; }
+
+    /// <summary>Status is the status of the condition. Can be True, False, Unknown.</summary>
+    [JsonPropertyName("status")]
+    public string? Status { get; set; }
+
+    /// <summary>Type is the type of the condition.</summary>
+    [JsonPropertyName("type")]
+    public string? Type { get; set; }
+}
+
+/// <summary>Output only. Metadata set during conversation data import.</summary>
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+public partial class V1alpha1DialogflowConversationDatasetStatusObservedStateConversationInfo
+{
+    /// <summary>Optional. The language code of the conversation data within this dataset. See https://cloud.google.com/apis/design/standard_fields for more information. Supports all UTF-8 languages.</summary>
+    [JsonPropertyName("languageCode")]
+    public string? LanguageCode { get; set; }
+}
+
+/// <summary>The Cloud Storage URI has the form gs://&lt;Google Cloud Storage bucket name&gt;//agent*.json. Wildcards are allowed and will be expanded into all matched JSON files, which will be read as one conversation per file.</summary>
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+public partial class V1alpha1DialogflowConversationDatasetStatusObservedStateInputConfigGcsSource
+{
+    /// <summary>Required. Google Cloud Storage URIs for the inputs. A URI is of the form: `gs://bucket/object-prefix-or-name` Whether a prefix or name is used depends on the use case.</summary>
+    [JsonPropertyName("uris")]
+    public IList<string>? Uris { get; set; }
+}
+
+/// <summary>Output only. Input configurations set during conversation data import.</summary>
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+public partial class V1alpha1DialogflowConversationDatasetStatusObservedStateInputConfig
+{
+    /// <summary>The Cloud Storage URI has the form gs://&lt;Google Cloud Storage bucket name&gt;//agent*.json. Wildcards are allowed and will be expanded into all matched JSON files, which will be read as one conversation per file.</summary>
+    [JsonPropertyName("gcsSource")]
+    public V1alpha1DialogflowConversationDatasetStatusObservedStateInputConfigGcsSource? GcsSource { get; set; }
+}
+
+/// <summary>ObservedState is the state of the resource as most recently observed in GCP.</summary>
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+public partial class V1alpha1DialogflowConversationDatasetStatusObservedState
+{
+    /// <summary>Output only. The number of conversations this conversation dataset contains.</summary>
+    [JsonPropertyName("conversationCount")]
+    public long? ConversationCount { get; set; }
+
+    /// <summary>Output only. Metadata set during conversation data import.</summary>
+    [JsonPropertyName("conversationInfo")]
+    public V1alpha1DialogflowConversationDatasetStatusObservedStateConversationInfo? ConversationInfo { get; set; }
+
+    /// <summary>Output only. Creation time of this dataset.</summary>
+    [JsonPropertyName("createTime")]
+    public string? CreateTime { get; set; }
+
+    /// <summary>Output only. Input configurations set during conversation data import.</summary>
+    [JsonPropertyName("inputConfig")]
+    public V1alpha1DialogflowConversationDatasetStatusObservedStateInputConfig? InputConfig { get; set; }
+
+    /// <summary>Output only. A read only boolean field reflecting Zone Isolation status of the dataset.</summary>
+    [JsonPropertyName("satisfiesPzi")]
+    public bool? SatisfiesPzi { get; set; }
+
+    /// <summary>Output only. A read only boolean field reflecting Zone Separation status of the dataset.</summary>
+    [JsonPropertyName("satisfiesPzs")]
+    public bool? SatisfiesPzs { get; set; }
+}
+
+/// <summary>DialogflowConversationDatasetStatus defines the config connector machine state of DialogflowConversationDataset</summary>
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+public partial class V1alpha1DialogflowConversationDatasetStatus
+{
+    /// <summary>Conditions represent the latest available observations of the object&apos;s current state.</summary>
+    [JsonPropertyName("conditions")]
+    public IList<V1alpha1DialogflowConversationDatasetStatusConditions>? Conditions { get; set; }
+
+    /// <summary>A unique specifier for the DialogflowConversationDataset resource in GCP.</summary>
+    [JsonPropertyName("externalRef")]
+    public string? ExternalRef { get; set; }
+
+    /// <summary>ObservedGeneration is the generation of the resource that was most recently observed by the Config Connector controller. If this is equal to metadata.generation, then that means that the current reported status reflects the most recent desired state of the resource.</summary>
+    [JsonPropertyName("observedGeneration")]
+    public long? ObservedGeneration { get; set; }
+
+    /// <summary>ObservedState is the state of the resource as most recently observed in GCP.</summary>
+    [JsonPropertyName("observedState")]
+    public V1alpha1DialogflowConversationDatasetStatusObservedState? ObservedState { get; set; }
+}
+
+/// <summary>DialogflowConversationDataset is the Schema for the DialogflowConversationDataset API</summary>
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+[KubernetesEntity(Group = KubeGroup, Kind = KubeKind, ApiVersion = KubeApiVersion, PluralName = KubePluralName)]
+public partial class V1alpha1DialogflowConversationDataset : IKubernetesObject<V1ObjectMeta>, ISpec<V1alpha1DialogflowConversationDatasetSpec>, IStatus<V1alpha1DialogflowConversationDatasetStatus?>
+{
+    public const string KubeApiVersion = "v1alpha1";
+    public const string KubeKind = "DialogflowConversationDataset";
+    public const string KubeGroup = "dialogflow.cnrm.cloud.google.com";
+    public const string KubePluralName = "dialogflowconversationdatasets";
+    /// <summary>APIVersion defines the versioned schema of this representation of an object. Servers should convert recognized schemas to the latest internal value, and may reject unrecognized values. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources</summary>
+    [JsonPropertyName("apiVersion")]
+    public string ApiVersion { get; set; } = "dialogflow.cnrm.cloud.google.com/v1alpha1";
+
+    /// <summary>Kind is a string value representing the REST resource this object represents. Servers may infer this from the endpoint the client submits requests to. Cannot be updated. In CamelCase. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds</summary>
+    [JsonPropertyName("kind")]
+    public string Kind { get; set; } = "DialogflowConversationDataset";
+
+    /// <summary>Standard object&apos;s metadata. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#metadata</summary>
+    [JsonPropertyName("metadata")]
+    public V1ObjectMeta Metadata { get; set; }
+
+    /// <summary>DialogflowConversationDatasetSpec defines the desired state of DialogflowConversationDataset</summary>
+    [JsonPropertyName("spec")]
+    public required V1alpha1DialogflowConversationDatasetSpec Spec { get; set; }
+
+    /// <summary>DialogflowConversationDatasetStatus defines the config connector machine state of DialogflowConversationDataset</summary>
+    [JsonPropertyName("status")]
+    public V1alpha1DialogflowConversationDatasetStatus? Status { get; set; }
+}
