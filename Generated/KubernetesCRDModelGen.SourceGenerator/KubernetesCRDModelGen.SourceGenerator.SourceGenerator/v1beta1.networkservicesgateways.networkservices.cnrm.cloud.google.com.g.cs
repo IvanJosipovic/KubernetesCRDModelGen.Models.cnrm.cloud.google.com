59eda9fb973e3250f4681053d3302f1d92534267
IvanJosipovic <9521987+IvanJosipovic@users.noreply.github.com>
2026-06-17 17:56:14 +0000

    feat: Update CRDs

--- Generated/KubernetesCRDModelGen.SourceGenerator/KubernetesCRDModelGen.SourceGenerator.SourceGenerator/v1beta1.networkservicesgateways.networkservices.cnrm.cloud.google.com.g.cs
+++ Generated/KubernetesCRDModelGen.SourceGenerator/KubernetesCRDModelGen.SourceGenerator.SourceGenerator/v1beta1.networkservicesgateways.networkservices.cnrm.cloud.google.com.g.cs
@@ -9,6 +9,7 @@ using System.Text.Json.Nodes;
 using System.Text.Json.Serialization;
 
 namespace KubernetesCRDModelGen.Models.networkservices.cnrm.cloud.google.com;
+/// <summary>NetworkServicesGateway is the Schema for the NetworkServicesGateway API</summary>
 [global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
 [global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
 [KubernetesEntity(Group = KubeGroup, Kind = KubeKind, ApiVersion = KubeApiVersion, PluralName = KubePluralName)]
@@ -40,49 +41,50 @@ public partial class V1beta1NetworkServicesGatewayList : IKubernetesObject<V1Lis
 [global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
 public partial class V1beta1NetworkServicesGatewaySpecProjectRef
 {
-    /// <summary>
-    /// The project for the resource
-    /// 
-    /// Allowed value: The Google Cloud resource name of a `Project` resource (format: `projects/{{name}}`).
-    /// </summary>
+    /// <summary>The `projectID` field of a project, when not managed by Config Connector.</summary>
     [JsonPropertyName("external")]
     public string? External { get; set; }
 
-    /// <summary>Name of the referent. More info: https://kubernetes.io/docs/concepts/overview/working-with-objects/names/#names</summary>
+    /// <summary>The `name` field of a `Project` resource.</summary>
     [JsonPropertyName("name")]
     public string? Name { get; set; }
 
-    /// <summary>Namespace of the referent. More info: https://kubernetes.io/docs/concepts/overview/working-with-objects/namespaces/</summary>
+    /// <summary>The `namespace` field of a `Project` resource.</summary>
     [JsonPropertyName("namespace")]
     public string? Namespace { get; set; }
 }
 
+/// <summary>Optional. A fully-qualified ServerTLSPolicy URL reference. Specifies how TLS traffic is terminated. If empty, TLS termination is disabled.</summary>
 [global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
 [global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
 public partial class V1beta1NetworkServicesGatewaySpecServerTlsPolicyRef
 {
-    /// <summary>
-    /// Optional. A fully-qualified ServerTLSPolicy URL reference. Specifies how TLS traffic is terminated. If empty, TLS termination is disabled.
-    /// 
-    /// Allowed value: The Google Cloud resource name of a `NetworkSecurityServerTLSPolicy` resource (format: `projects/{{project}}/locations/{{location}}/serverTlsPolicies/{{name}}`).
-    /// </summary>
+    /// <summary>Allowed value: string of the format `projects/{{project}}/locations/{{location}}/serverTlsPolicies/{{value}}`, where {{value}} is the `name` field of a `NetworkSecurityServerTLSPolicy` resource.</summary>
     [JsonPropertyName("external")]
     public string? External { get; set; }
 
-    /// <summary>Name of the referent. More info: https://kubernetes.io/docs/concepts/overview/working-with-objects/names/#names</summary>
+    /// <summary>The name of a NetworkSecurityServerTLSPolicy resource.</summary>
     [JsonPropertyName("name")]
     public string? Name { get; set; }
 
-    /// <summary>Namespace of the referent. More info: https://kubernetes.io/docs/concepts/overview/working-with-objects/namespaces/</summary>
+    /// <summary>The namespace of a NetworkSecurityServerTLSPolicy resource.</summary>
     [JsonPropertyName("namespace")]
     public string? Namespace { get; set; }
 }
 
+/// <summary>NetworkServicesGatewaySpec defines the desired state of NetworkServicesGateway</summary>
 [global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
 [global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
 public partial class V1beta1NetworkServicesGatewaySpec
 {
-    /// <summary>One or more addresses with ports in format of &quot;:&quot; that the Gateway must receive traffic on. The proxy binds to the ports specified. IP address can be anything that is allowed by the underlying infrastructure (auto-allocation, static IP, BYOIP).</summary>
+    /// <summary>
+    /// Optional. Zero or one IPv4 or IPv6 address on which the Gateway will
+    ///  receive the traffic. When no address is provided, an IP from the subnetwork
+    ///  is allocated
+    /// 
+    ///  This field only applies to gateways of type &apos;SECURE_WEB_GATEWAY&apos;.
+    ///  Gateways of type &apos;OPEN_MESH&apos; listen on 0.0.0.0 for IPv4 and :: for IPv6.
+    /// </summary>
     [JsonPropertyName("addresses")]
     public IList<string>? Addresses { get; set; }
 
@@ -90,7 +92,7 @@ public partial class V1beta1NetworkServicesGatewaySpec
     [JsonPropertyName("description")]
     public string? Description { get; set; }
 
-    /// <summary>Immutable. The location for the resource</summary>
+    /// <summary>Immutable. The location for the resource.</summary>
     [JsonPropertyName("location")]
     public required string Location { get; set; }
 
@@ -110,6 +112,7 @@ public partial class V1beta1NetworkServicesGatewaySpec
     [JsonPropertyName("scope")]
     public required string Scope { get; set; }
 
+    /// <summary>Optional. A fully-qualified ServerTLSPolicy URL reference. Specifies how TLS traffic is terminated. If empty, TLS termination is disabled.</summary>
     [JsonPropertyName("serverTlsPolicyRef")]
     public V1beta1NetworkServicesGatewaySpecServerTlsPolicyRef? ServerTlsPolicyRef { get; set; }
 
@@ -143,11 +146,12 @@ public partial class V1beta1NetworkServicesGatewayStatusConditions
     public string? Type { get; set; }
 }
 
+/// <summary>NetworkServicesGatewayStatus defines the config connector machine state of NetworkServicesGateway</summary>
 [global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
 [global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
 public partial class V1beta1NetworkServicesGatewayStatus
 {
-    /// <summary>Conditions represent the latest available observation of the resource&apos;s current state.</summary>
+    /// <summary>Conditions represent the latest available observations of the object&apos;s current state.</summary>
     [JsonPropertyName("conditions")]
     public IList<V1beta1NetworkServicesGatewayStatusConditions>? Conditions { get; set; }
 
@@ -157,7 +161,7 @@ public partial class V1beta1NetworkServicesGatewayStatus
 
     /// <summary>ObservedGeneration is the generation of the resource that was most recently observed by the Config Connector controller. If this is equal to metadata.generation, then that means that the current reported status reflects the most recent desired state of the resource.</summary>
     [JsonPropertyName("observedGeneration")]
-    public int? ObservedGeneration { get; set; }
+    public long? ObservedGeneration { get; set; }
 
     /// <summary>Output only. Server-defined URL of this resource</summary>
     [JsonPropertyName("selfLink")]
@@ -168,6 +172,7 @@ public partial class V1beta1NetworkServicesGatewayStatus
     public DateTime? UpdateTime { get; set; }
 }
 
+/// <summary>NetworkServicesGateway is the Schema for the NetworkServicesGateway API</summary>
 [global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
 [global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
 [KubernetesEntity(Group = KubeGroup, Kind = KubeKind, ApiVersion = KubeApiVersion, PluralName = KubePluralName)]
@@ -189,9 +194,11 @@ public partial class V1beta1NetworkServicesGateway : IKubernetesObject<V1ObjectM
     [JsonPropertyName("metadata")]
     public V1ObjectMeta Metadata { get; set; }
 
+    /// <summary>NetworkServicesGatewaySpec defines the desired state of NetworkServicesGateway</summary>
     [JsonPropertyName("spec")]
     public required V1beta1NetworkServicesGatewaySpec Spec { get; set; }
 
+    /// <summary>NetworkServicesGatewayStatus defines the config connector machine state of NetworkServicesGateway</summary>
     [JsonPropertyName("status")]
     public V1beta1NetworkServicesGatewayStatus? Status { get; set; }
 }
