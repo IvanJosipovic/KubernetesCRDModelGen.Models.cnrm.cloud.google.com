c9590ebe511469d3e2810967212b496251491360
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2026-06-16 18:50:02 -0700

    feat: Update CRDs (#192)

---
+++ Generated/KubernetesCRDModelGen.SourceGenerator/KubernetesCRDModelGen.SourceGenerator.SourceGenerator/v1alpha1.artifactregistryvpcscconfigs.artifactregistry.cnrm.cloud.google.com.g.cs
@@ -0,0 +1,166 @@
+﻿#nullable enable
+using k8s;
+using k8s.Models;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+
+namespace KubernetesCRDModelGen.Models.artifactregistry.cnrm.cloud.google.com;
+/// <summary>ArtifactRegistryVPCSCConfig is the Schema for the ArtifactRegistryVPCSCConfig API</summary>
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+[KubernetesEntity(Group = KubeGroup, Kind = KubeKind, ApiVersion = KubeApiVersion, PluralName = KubePluralName)]
+public partial class V1alpha1ArtifactRegistryVPCSCConfigList : IKubernetesObject<V1ListMeta>, IItems<V1alpha1ArtifactRegistryVPCSCConfig>
+{
+    public const string KubeApiVersion = "v1alpha1";
+    public const string KubeKind = "ArtifactRegistryVPCSCConfigList";
+    public const string KubeGroup = "artifactregistry.cnrm.cloud.google.com";
+    public const string KubePluralName = "artifactregistryvpcscconfigs";
+    /// <summary>APIVersion defines the versioned schema of this representation of an object. Servers should convert recognized schemas to the latest internal value, and may reject unrecognized values. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources</summary>
+    [JsonPropertyName("apiVersion")]
+    public string ApiVersion { get; set; } = "artifactregistry.cnrm.cloud.google.com/v1alpha1";
+
+    /// <summary>Kind is a string value representing the REST resource this object represents. Servers may infer this from the endpoint the client submits requests to. Cannot be updated. In CamelCase. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds</summary>
+    [JsonPropertyName("kind")]
+    public string Kind { get; set; } = "ArtifactRegistryVPCSCConfigList";
+
+    /// <summary>ListMeta describes metadata that synthetic resources must have, including lists and various status objects. A resource may have only one of {ObjectMeta, ListMeta}.</summary>
+    [JsonPropertyName("metadata")]
+    public V1ListMeta? Metadata { get; set; }
+
+    /// <summary>List of V1alpha1ArtifactRegistryVPCSCConfig objects.</summary>
+    [JsonPropertyName("items")]
+    public required IList<V1alpha1ArtifactRegistryVPCSCConfig> Items { get; set; }
+}
+
+/// <summary>The project that this resource belongs to.</summary>
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+public partial class V1alpha1ArtifactRegistryVPCSCConfigSpecProjectRef
+{
+    /// <summary>The `projectID` field of a project, when not managed by Config Connector.</summary>
+    [JsonPropertyName("external")]
+    public string? External { get; set; }
+
+    /// <summary>The kind of the Project resource; optional but must be `Project` if provided.</summary>
+    [JsonPropertyName("kind")]
+    public string? Kind { get; set; }
+
+    /// <summary>The `name` field of a `Project` resource.</summary>
+    [JsonPropertyName("name")]
+    public string? Name { get; set; }
+
+    /// <summary>The `namespace` field of a `Project` resource.</summary>
+    [JsonPropertyName("namespace")]
+    public string? Namespace { get; set; }
+}
+
+/// <summary>ArtifactRegistryVPCSCConfigSpec defines the desired state of ArtifactRegistryVPCSCConfig</summary>
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+public partial class V1alpha1ArtifactRegistryVPCSCConfigSpec
+{
+    /// <summary>The location of this resource.</summary>
+    [JsonPropertyName("location")]
+    public required string Location { get; set; }
+
+    /// <summary>The project that this resource belongs to.</summary>
+    [JsonPropertyName("projectRef")]
+    public required V1alpha1ArtifactRegistryVPCSCConfigSpecProjectRef ProjectRef { get; set; }
+
+    /// <summary>The ArtifactRegistryVPCSCConfig name. If not given, the metadata.name will be used.</summary>
+    [JsonPropertyName("resourceID")]
+    public string? ResourceID { get; set; }
+
+    /// <summary>The VPC SC policy for project and location. Possible values: &quot;VPCSC_POLICY_UNSPECIFIED&quot; - the VPS SC policy is not defined. &quot;DENY&quot; - repository will block the requests to the Upstreams for the Remote Repositories if the resource is in the perimeter. &quot;ALLOW&quot; - repository will allow the requests to the Upstreams for the Remote Repositories if the resource is in the perimeter.</summary>
+    [JsonPropertyName("vpcscPolicy")]
+    public string? VpcscPolicy { get; set; }
+}
+
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+public partial class V1alpha1ArtifactRegistryVPCSCConfigStatusConditions
+{
+    /// <summary>Last time the condition transitioned from one status to another.</summary>
+    [JsonPropertyName("lastTransitionTime")]
+    public string? LastTransitionTime { get; set; }
+
+    /// <summary>Human-readable message indicating details about last transition.</summary>
+    [JsonPropertyName("message")]
+    public string? Message { get; set; }
+
+    /// <summary>Unique, one-word, CamelCase reason for the condition&apos;s last transition.</summary>
+    [JsonPropertyName("reason")]
+    public string? Reason { get; set; }
+
+    /// <summary>Status is the status of the condition. Can be True, False, Unknown.</summary>
+    [JsonPropertyName("status")]
+    public string? Status { get; set; }
+
+    /// <summary>Type is the type of the condition.</summary>
+    [JsonPropertyName("type")]
+    public string? Type { get; set; }
+}
+
+/// <summary>ObservedState is the state of the resource as most recently observed in GCP.</summary>
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+public partial class V1alpha1ArtifactRegistryVPCSCConfigStatusObservedState
+{
+}
+
+/// <summary>ArtifactRegistryVPCSCConfigStatus defines the config connector machine state of ArtifactRegistryVPCSCConfig</summary>
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+public partial class V1alpha1ArtifactRegistryVPCSCConfigStatus
+{
+    /// <summary>Conditions represent the latest available observations of the object&apos;s current state.</summary>
+    [JsonPropertyName("conditions")]
+    public IList<V1alpha1ArtifactRegistryVPCSCConfigStatusConditions>? Conditions { get; set; }
+
+    /// <summary>A unique specifier for the ArtifactRegistryVPCSCConfig resource in GCP.</summary>
+    [JsonPropertyName("externalRef")]
+    public string? ExternalRef { get; set; }
+
+    /// <summary>ObservedGeneration is the generation of the resource that was most recently observed by the Config Connector controller. If this is equal to metadata.generation, then that means that the current reported status reflects the most recent desired state of the resource.</summary>
+    [JsonPropertyName("observedGeneration")]
+    public long? ObservedGeneration { get; set; }
+
+    /// <summary>ObservedState is the state of the resource as most recently observed in GCP.</summary>
+    [JsonPropertyName("observedState")]
+    public V1alpha1ArtifactRegistryVPCSCConfigStatusObservedState? ObservedState { get; set; }
+}
+
+/// <summary>ArtifactRegistryVPCSCConfig is the Schema for the ArtifactRegistryVPCSCConfig API</summary>
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+[KubernetesEntity(Group = KubeGroup, Kind = KubeKind, ApiVersion = KubeApiVersion, PluralName = KubePluralName)]
+public partial class V1alpha1ArtifactRegistryVPCSCConfig : IKubernetesObject<V1ObjectMeta>, ISpec<V1alpha1ArtifactRegistryVPCSCConfigSpec>, IStatus<V1alpha1ArtifactRegistryVPCSCConfigStatus?>
+{
+    public const string KubeApiVersion = "v1alpha1";
+    public const string KubeKind = "ArtifactRegistryVPCSCConfig";
+    public const string KubeGroup = "artifactregistry.cnrm.cloud.google.com";
+    public const string KubePluralName = "artifactregistryvpcscconfigs";
+    /// <summary>APIVersion defines the versioned schema of this representation of an object. Servers should convert recognized schemas to the latest internal value, and may reject unrecognized values. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources</summary>
+    [JsonPropertyName("apiVersion")]
+    public string ApiVersion { get; set; } = "artifactregistry.cnrm.cloud.google.com/v1alpha1";
+
+    /// <summary>Kind is a string value representing the REST resource this object represents. Servers may infer this from the endpoint the client submits requests to. Cannot be updated. In CamelCase. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds</summary>
+    [JsonPropertyName("kind")]
+    public string Kind { get; set; } = "ArtifactRegistryVPCSCConfig";
+
+    /// <summary>Standard object&apos;s metadata. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#metadata</summary>
+    [JsonPropertyName("metadata")]
+    public V1ObjectMeta Metadata { get; set; }
+
+    /// <summary>ArtifactRegistryVPCSCConfigSpec defines the desired state of ArtifactRegistryVPCSCConfig</summary>
+    [JsonPropertyName("spec")]
+    public required V1alpha1ArtifactRegistryVPCSCConfigSpec Spec { get; set; }
+
+    /// <summary>ArtifactRegistryVPCSCConfigStatus defines the config connector machine state of ArtifactRegistryVPCSCConfig</summary>
+    [JsonPropertyName("status")]
+    public V1alpha1ArtifactRegistryVPCSCConfigStatus? Status { get; set; }
+}
