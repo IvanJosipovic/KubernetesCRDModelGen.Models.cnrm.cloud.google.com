bd651043bc83c22285835aec3d29a58255e84a78
IvanJosipovic <9521987+IvanJosipovic@users.noreply.github.com>
2026-06-17 21:41:22 +0000

    feat: Update CRDs

--- Generated/KubernetesCRDModelGen.SourceGenerator/KubernetesCRDModelGen.SourceGenerator.SourceGenerator/v1beta1.computehealthchecks.compute.cnrm.cloud.google.com.g.cs
+++ Generated/KubernetesCRDModelGen.SourceGenerator/KubernetesCRDModelGen.SourceGenerator.SourceGenerator/v1beta1.computehealthchecks.compute.cnrm.cloud.google.com.g.cs
@@ -265,6 +265,28 @@ public partial class V1beta1ComputeHealthCheckSpec
     [JsonPropertyName("resourceID")]
     public string? ResourceID { get; set; }
 
+    /// <summary>
+    /// The list of cloud regions from which health checks are performed. If
+    /// any regions are specified, then exactly 3 regions should be specified.
+    /// The region names must be valid names of Google Cloud regions. This can
+    /// only be set for global health check. If this list is non-empty, then
+    /// there are restrictions on what other health check fields are supported
+    /// and what other resources can use this health check:
+    /// 
+    /// * SSL, HTTP2, and GRPC protocols are not supported.
+    /// 
+    /// * The TCP request field is not supported.
+    /// 
+    /// * The proxyHeader field for HTTP, HTTPS, and TCP is not supported.
+    /// 
+    /// * The checkIntervalSec field must be at least 30.
+    /// 
+    /// * The health check cannot be used with BackendService nor with managed
+    /// instance group auto-healing.
+    /// </summary>
+    [JsonPropertyName("sourceRegions")]
+    public IList<string>? SourceRegions { get; set; }
+
     [JsonPropertyName("sslHealthCheck")]
     public V1beta1ComputeHealthCheckSpecSslHealthCheck? SslHealthCheck { get; set; }
 
