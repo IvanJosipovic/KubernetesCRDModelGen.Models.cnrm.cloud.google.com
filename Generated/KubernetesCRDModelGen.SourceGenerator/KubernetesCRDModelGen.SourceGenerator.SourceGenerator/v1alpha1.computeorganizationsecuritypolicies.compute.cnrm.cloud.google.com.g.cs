66ffd335f2c9dfdf54c65a99652584feb64608bd
IvanJosipovic <9521987+IvanJosipovic@users.noreply.github.com>
2026-06-23 12:00:44 +0000

    feat: Update CRDs

--- Generated/KubernetesCRDModelGen.SourceGenerator/KubernetesCRDModelGen.SourceGenerator.SourceGenerator/v1alpha1.computeorganizationsecuritypolicies.compute.cnrm.cloud.google.com.g.cs
+++ Generated/KubernetesCRDModelGen.SourceGenerator/KubernetesCRDModelGen.SourceGenerator.SourceGenerator/v1alpha1.computeorganizationsecuritypolicies.compute.cnrm.cloud.google.com.g.cs
@@ -9,6 +9,7 @@ using System.Text.Json.Nodes;
 using System.Text.Json.Serialization;
 
 namespace KubernetesCRDModelGen.Models.compute.cnrm.cloud.google.com;
+/// <summary>ComputeOrganizationSecurityPolicy is the Schema for the compute API</summary>
 [global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
 [global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
 [KubernetesEntity(Group = KubeGroup, Kind = KubeKind, ApiVersion = KubeApiVersion, PluralName = KubePluralName)]
@@ -35,6 +36,7 @@ public partial class V1alpha1ComputeOrganizationSecurityPolicyList : IKubernetes
     public required IList<V1alpha1ComputeOrganizationSecurityPolicy> Items { get; set; }
 }
 
+/// <summary>ComputeOrganizationSecurityPolicySpec defines the desired state of ComputeOrganizationSecurityPolicy</summary>
 [global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
 [global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
 public partial class V1alpha1ComputeOrganizationSecurityPolicySpec
@@ -47,10 +49,7 @@ public partial class V1alpha1ComputeOrganizationSecurityPolicySpec
     [JsonPropertyName("displayName")]
     public required string DisplayName { get; set; }
 
-    /// <summary>
-    /// Immutable. The parent of this OrganizationSecurityPolicy in the Cloud Resource Hierarchy.
-    /// Format: organizations/{organization_id} or folders/{folder_id}.
-    /// </summary>
+    /// <summary>Immutable. The parent of this OrganizationSecurityPolicy in the Cloud Resource Hierarchy. Format: organizations/{organization_id} or folders/{folder_id}.</summary>
     [JsonPropertyName("parent")]
     public required string Parent { get; set; }
 
@@ -58,11 +57,7 @@ public partial class V1alpha1ComputeOrganizationSecurityPolicySpec
     [JsonPropertyName("resourceID")]
     public string? ResourceID { get; set; }
 
-    /// <summary>
-    /// Immutable. The type indicates the intended use of the security policy.
-    /// For organization security policies, the only supported type
-    /// is &quot;FIREWALL&quot;. Default value: &quot;FIREWALL&quot; Possible values: [&quot;FIREWALL&quot;].
-    /// </summary>
+    /// <summary>Immutable. The type indicates the intended use of the security policy. For organization security policies, the only supported type is &quot;FIREWALL&quot;. Default value: &quot;FIREWALL&quot; Possible values: [&quot;FIREWALL&quot;].</summary>
     [JsonPropertyName("type")]
     public string? Type { get; set; }
 }
@@ -92,30 +87,29 @@ public partial class V1alpha1ComputeOrganizationSecurityPolicyStatusConditions
     public string? Type { get; set; }
 }
 
+/// <summary>ComputeOrganizationSecurityPolicyStatus defines the config connector machine state of ComputeOrganizationSecurityPolicy</summary>
 [global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
 [global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
 public partial class V1alpha1ComputeOrganizationSecurityPolicyStatus
 {
-    /// <summary>Conditions represent the latest available observation of the resource&apos;s current state.</summary>
+    /// <summary>Conditions represent the latest available observations of the ComputeOrganizationSecurityPolicy&apos;s current state.</summary>
     [JsonPropertyName("conditions")]
     public IList<V1alpha1ComputeOrganizationSecurityPolicyStatusConditions>? Conditions { get; set; }
 
-    /// <summary>
-    /// Fingerprint of this resource. This field is used internally during
-    /// updates of this resource.
-    /// </summary>
+    /// <summary>Fingerprint of this resource. This field is used internally during updates of this resource.</summary>
     [JsonPropertyName("fingerprint")]
     public string? Fingerprint { get; set; }
 
     /// <summary>ObservedGeneration is the generation of the resource that was most recently observed by the Config Connector controller. If this is equal to metadata.generation, then that means that the current reported status reflects the most recent desired state of the resource.</summary>
     [JsonPropertyName("observedGeneration")]
-    public int? ObservedGeneration { get; set; }
+    public long? ObservedGeneration { get; set; }
 
     /// <summary>The unique identifier for the resource. This identifier is defined by the server.</summary>
     [JsonPropertyName("policyId")]
     public string? PolicyId { get; set; }
 }
 
+/// <summary>ComputeOrganizationSecurityPolicy is the Schema for the compute API</summary>
 [global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
 [global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
 [KubernetesEntity(Group = KubeGroup, Kind = KubeKind, ApiVersion = KubeApiVersion, PluralName = KubePluralName)]
@@ -137,9 +131,11 @@ public partial class V1alpha1ComputeOrganizationSecurityPolicy : IKubernetesObje
     [JsonPropertyName("metadata")]
     public V1ObjectMeta Metadata { get; set; }
 
+    /// <summary>ComputeOrganizationSecurityPolicySpec defines the desired state of ComputeOrganizationSecurityPolicy</summary>
     [JsonPropertyName("spec")]
     public required V1alpha1ComputeOrganizationSecurityPolicySpec Spec { get; set; }
 
+    /// <summary>ComputeOrganizationSecurityPolicyStatus defines the config connector machine state of ComputeOrganizationSecurityPolicy</summary>
     [JsonPropertyName("status")]
     public V1alpha1ComputeOrganizationSecurityPolicyStatus? Status { get; set; }
 }
