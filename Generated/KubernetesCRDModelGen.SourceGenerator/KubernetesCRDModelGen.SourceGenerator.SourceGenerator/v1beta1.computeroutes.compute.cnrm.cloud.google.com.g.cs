586f354dab64d352d5d734645c87c77f92e61727
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2026-06-24 03:05:56 -0700

    feat: Update CRDs (#236)

--- Generated/KubernetesCRDModelGen.SourceGenerator/KubernetesCRDModelGen.SourceGenerator.SourceGenerator/v1beta1.computeroutes.compute.cnrm.cloud.google.com.g.cs
+++ Generated/KubernetesCRDModelGen.SourceGenerator/KubernetesCRDModelGen.SourceGenerator.SourceGenerator/v1beta1.computeroutes.compute.cnrm.cloud.google.com.g.cs
@@ -9,6 +9,7 @@ using System.Text.Json.Nodes;
 using System.Text.Json.Serialization;
 
 namespace KubernetesCRDModelGen.Models.compute.cnrm.cloud.google.com;
+/// <summary>ComputeRoute is the Schema for the ComputeRoute API</summary>
 [global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
 [global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
 [KubernetesEntity(Group = KubeGroup, Kind = KubeKind, ApiVersion = KubeApiVersion, PluralName = KubePluralName)]
@@ -40,37 +41,33 @@ public partial class V1beta1ComputeRouteList : IKubernetesObject<V1ListMeta>, II
 [global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
 public partial class V1beta1ComputeRouteSpecNetworkRef
 {
-    /// <summary>Allowed value: The `selfLink` field of a `ComputeNetwork` resource.</summary>
+    /// <summary>A reference to an externally managed ComputeNetwork resource. Should be in the format &quot;projects/{{projectID}}/global/networks/{{networkID}}&quot;.</summary>
     [JsonPropertyName("external")]
     public string? External { get; set; }
 
-    /// <summary>Name of the referent. More info: https://kubernetes.io/docs/concepts/overview/working-with-objects/names/#names</summary>
+    /// <summary>The name of a ComputeNetwork resource.</summary>
     [JsonPropertyName("name")]
     public string? Name { get; set; }
 
-    /// <summary>Namespace of the referent. More info: https://kubernetes.io/docs/concepts/overview/working-with-objects/namespaces/</summary>
+    /// <summary>The namespace of a ComputeNetwork resource.</summary>
     [JsonPropertyName("namespace")]
     public string? Namespace { get; set; }
 }
 
-/// <summary>
-/// A forwarding rule of type loadBalancingScheme=INTERNAL that should
-/// handle matching packets.  Note that this can only be used when the
-/// destinationRange is a public (non-RFC 1918) IP CIDR range.
-/// </summary>
+/// <summary>A forwarding rule of type loadBalancingScheme=INTERNAL that should handle matching packets.  Note that this can only be used when the destinationRange is a public (non-RFC 1918) IP CIDR range.</summary>
 [global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
 [global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
 public partial class V1beta1ComputeRouteSpecNextHopILBRef
 {
-    /// <summary>Allowed value: The `selfLink` field of a `ComputeForwardingRule` resource.</summary>
+    /// <summary>A reference to an externally managed ComputeForwardingRule resource. Should be in the format &quot;projects/{{projectID}}/global/forwardingRules/{{forwardingRuleID}}&quot; or &quot;projects/{{projectID}}/regions/{{region}}/forwardingRules/{{forwardingRuleID}}&quot;.</summary>
     [JsonPropertyName("external")]
     public string? External { get; set; }
 
-    /// <summary>Name of the referent. More info: https://kubernetes.io/docs/concepts/overview/working-with-objects/names/#names</summary>
+    /// <summary>The name of a ComputeForwardingRule resource.</summary>
     [JsonPropertyName("name")]
     public string? Name { get; set; }
 
-    /// <summary>Namespace of the referent. More info: https://kubernetes.io/docs/concepts/overview/working-with-objects/namespaces/</summary>
+    /// <summary>The namespace of a ComputeForwardingRule resource.</summary>
     [JsonPropertyName("namespace")]
     public string? Namespace { get; set; }
 }
@@ -80,15 +77,15 @@ public partial class V1beta1ComputeRouteSpecNextHopILBRef
 [global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
 public partial class V1beta1ComputeRouteSpecNextHopInstanceRef
 {
-    /// <summary>Allowed value: The `selfLink` field of a `ComputeInstance` resource.</summary>
+    /// <summary>A reference to an externally managed ComputeInstance resource. Should be in the format &quot;projects/{{projectID}}/zones/{{zone}}/instances/{{instanceID}}&quot;.</summary>
     [JsonPropertyName("external")]
     public string? External { get; set; }
 
-    /// <summary>Name of the referent. More info: https://kubernetes.io/docs/concepts/overview/working-with-objects/names/#names</summary>
+    /// <summary>The name of a ComputeInstance resource.</summary>
     [JsonPropertyName("name")]
     public string? Name { get; set; }
 
-    /// <summary>Namespace of the referent. More info: https://kubernetes.io/docs/concepts/overview/working-with-objects/namespaces/</summary>
+    /// <summary>The namespace of a ComputeInstance resource.</summary>
     [JsonPropertyName("namespace")]
     public string? Namespace { get; set; }
 }
@@ -111,21 +108,16 @@ public partial class V1beta1ComputeRouteSpecNextHopVPNTunnelRef
     public string? Namespace { get; set; }
 }
 
+/// <summary>ComputeRouteSpec defines the desired state of ComputeRoute</summary>
 [global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
 [global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
 public partial class V1beta1ComputeRouteSpec
 {
-    /// <summary>
-    /// Immutable. An optional description of this resource. Provide this property
-    /// when you create the resource.
-    /// </summary>
+    /// <summary>Immutable. An optional description of this resource. Provide this property when you create the resource.</summary>
     [JsonPropertyName("description")]
     public string? Description { get; set; }
 
-    /// <summary>
-    /// Immutable. The destination range of outgoing packets that this route applies to.
-    /// Only IPv4 is supported.
-    /// </summary>
+    /// <summary>Immutable. The destination range of outgoing packets that this route applies to. Only IPv4 is supported.</summary>
     [JsonPropertyName("destRange")]
     public required string DestRange { get; set; }
 
@@ -133,23 +125,11 @@ public partial class V1beta1ComputeRouteSpec
     [JsonPropertyName("networkRef")]
     public required V1beta1ComputeRouteSpecNetworkRef NetworkRef { get; set; }
 
-    /// <summary>
-    /// Immutable. URL to a gateway that should handle matching packets.
-    /// Currently, you can only specify the internet gateway, using a full or
-    /// partial valid URL:
-    /// * &apos;https://www.googleapis.com/compute/v1/projects/project/global/gateways/default-internet-gateway&apos;
-    /// * &apos;projects/project/global/gateways/default-internet-gateway&apos;
-    /// * &apos;global/gateways/default-internet-gateway&apos;
-    /// * The string &apos;default-internet-gateway&apos;.
-    /// </summary>
+    /// <summary>Immutable. URL to a gateway that should handle matching packets. Currently, you can only specify the internet gateway, using a full or partial valid URL: * &apos;https://www.googleapis.com/compute/v1/projects/project/global/gateways/default-internet-gateway&apos; * &apos;projects/project/global/gateways/default-internet-gateway&apos; * &apos;global/gateways/default-internet-gateway&apos; * The string &apos;default-internet-gateway&apos;.</summary>
     [JsonPropertyName("nextHopGateway")]
     public string? NextHopGateway { get; set; }
 
-    /// <summary>
-    /// A forwarding rule of type loadBalancingScheme=INTERNAL that should
-    /// handle matching packets.  Note that this can only be used when the
-    /// destinationRange is a public (non-RFC 1918) IP CIDR range.
-    /// </summary>
+    /// <summary>A forwarding rule of type loadBalancingScheme=INTERNAL that should handle matching packets.  Note that this can only be used when the destinationRange is a public (non-RFC 1918) IP CIDR range.</summary>
     [JsonPropertyName("nextHopILBRef")]
     public V1beta1ComputeRouteSpecNextHopILBRef? NextHopILBRef { get; set; }
 
@@ -211,11 +191,12 @@ public partial class V1beta1ComputeRouteStatusConditions
     public string? Type { get; set; }
 }
 
+/// <summary>ComputeRouteStatus defines the config connector machine state of ComputeRoute</summary>
 [global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
 [global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
 public partial class V1beta1ComputeRouteStatus
 {
-    /// <summary>Conditions represent the latest available observation of the resource&apos;s current state.</summary>
+    /// <summary>Conditions represent the latest available observations of the object&apos;s current state.</summary>
     [JsonPropertyName("conditions")]
     public IList<V1beta1ComputeRouteStatusConditions>? Conditions { get; set; }
 
@@ -225,12 +206,13 @@ public partial class V1beta1ComputeRouteStatus
 
     /// <summary>ObservedGeneration is the generation of the resource that was most recently observed by the Config Connector controller. If this is equal to metadata.generation, then that means that the current reported status reflects the most recent desired state of the resource.</summary>
     [JsonPropertyName("observedGeneration")]
-    public int? ObservedGeneration { get; set; }
+    public long? ObservedGeneration { get; set; }
 
     [JsonPropertyName("selfLink")]
     public string? SelfLink { get; set; }
 }
 
+/// <summary>ComputeRoute is the Schema for the ComputeRoute API</summary>
 [global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
 [global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
 [KubernetesEntity(Group = KubeGroup, Kind = KubeKind, ApiVersion = KubeApiVersion, PluralName = KubePluralName)]
@@ -252,9 +234,11 @@ public partial class V1beta1ComputeRoute : IKubernetesObject<V1ObjectMeta>, ISpe
     [JsonPropertyName("metadata")]
     public V1ObjectMeta Metadata { get; set; }
 
+    /// <summary>ComputeRouteSpec defines the desired state of ComputeRoute</summary>
     [JsonPropertyName("spec")]
     public required V1beta1ComputeRouteSpec Spec { get; set; }
 
+    /// <summary>ComputeRouteStatus defines the config connector machine state of ComputeRoute</summary>
     [JsonPropertyName("status")]
     public V1beta1ComputeRouteStatus? Status { get; set; }
 }
