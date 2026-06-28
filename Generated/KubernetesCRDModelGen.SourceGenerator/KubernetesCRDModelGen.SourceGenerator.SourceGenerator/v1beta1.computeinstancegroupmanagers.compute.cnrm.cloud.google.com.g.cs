4b4b5206d9048da5888f8735491c5a7c5488c604
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2026-06-28 01:08:51 -0700

    feat: Update CRDs (#245)

--- Generated/KubernetesCRDModelGen.SourceGenerator/KubernetesCRDModelGen.SourceGenerator.SourceGenerator/v1beta1.computeinstancegroupmanagers.compute.cnrm.cloud.google.com.g.cs
+++ Generated/KubernetesCRDModelGen.SourceGenerator/KubernetesCRDModelGen.SourceGenerator.SourceGenerator/v1beta1.computeinstancegroupmanagers.compute.cnrm.cloud.google.com.g.cs
@@ -9,6 +9,7 @@ using System.Text.Json.Nodes;
 using System.Text.Json.Serialization;
 
 namespace KubernetesCRDModelGen.Models.compute.cnrm.cloud.google.com;
+/// <summary>ComputeInstanceGroupManager is the Schema for the ComputeInstanceGroupManager API</summary>
 [global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
 [global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
 [KubernetesEntity(Group = KubeGroup, Kind = KubeKind, ApiVersion = KubeApiVersion, PluralName = KubePluralName)]
@@ -130,19 +131,15 @@ public partial class V1beta1ComputeInstanceGroupManagerSpecNamedPorts
 [global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
 public partial class V1beta1ComputeInstanceGroupManagerSpecProjectRef
 {
-    /// <summary>
-    /// The project for the resource
-    /// 
-    /// Allowed value: The Google Cloud resource name of a `Project` resource (format: `projects/{{name}}`).
-    /// </summary>
+    /// <summary>The `projectID` field of a project, when not managed by Config Connector.</summary>
     [JsonPropertyName("external")]
     public string? External { get; set; }
 
-    /// <summary>Name of the referent. More info: https://kubernetes.io/docs/concepts/overview/working-with-objects/names/#names</summary>
+    /// <summary>The `name` field of a `Project` resource.</summary>
     [JsonPropertyName("name")]
     public string? Name { get; set; }
 
-    /// <summary>Namespace of the referent. More info: https://kubernetes.io/docs/concepts/overview/working-with-objects/namespaces/</summary>
+    /// <summary>The `namespace` field of a `Project` resource.</summary>
     [JsonPropertyName("namespace")]
     public string? Namespace { get; set; }
 }
@@ -359,6 +356,7 @@ public partial class V1beta1ComputeInstanceGroupManagerSpecVersions
     public V1beta1ComputeInstanceGroupManagerSpecVersionsTargetSize? TargetSize { get; set; }
 }
 
+/// <summary>ComputeInstanceGroupManagerSpec defines the desired state of ComputeInstanceGroupManager</summary>
 [global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
 [global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
 public partial class V1beta1ComputeInstanceGroupManagerSpec
@@ -585,7 +583,7 @@ public partial class V1beta1ComputeInstanceGroupManagerStatusUpdatePolicy
 [global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
 public partial class V1beta1ComputeInstanceGroupManagerStatus
 {
-    /// <summary>Conditions represent the latest available observation of the resource&apos;s current state.</summary>
+    /// <summary>Conditions represent the latest available observations of the ComputeInstanceGroupManager&apos;s current state.</summary>
     [JsonPropertyName("conditions")]
     public IList<V1beta1ComputeInstanceGroupManagerStatusConditions>? Conditions { get; set; }
 
@@ -611,7 +609,7 @@ public partial class V1beta1ComputeInstanceGroupManagerStatus
 
     /// <summary>ObservedGeneration is the generation of the resource that was most recently observed by the Config Connector controller. If this is equal to metadata.generation, then that means that the current reported status reflects the most recent desired state of the resource.</summary>
     [JsonPropertyName("observedGeneration")]
-    public int? ObservedGeneration { get; set; }
+    public long? ObservedGeneration { get; set; }
 
     /// <summary>[Output Only] The URL of the [region](/compute/docs/regions-zones/#available) where the managed instance group resides (for regional resources).</summary>
     [JsonPropertyName("region")]
@@ -633,6 +631,7 @@ public partial class V1beta1ComputeInstanceGroupManagerStatus
     public string? Zone { get; set; }
 }
 
+/// <summary>ComputeInstanceGroupManager is the Schema for the ComputeInstanceGroupManager API</summary>
 [global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
 [global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
 [KubernetesEntity(Group = KubeGroup, Kind = KubeKind, ApiVersion = KubeApiVersion, PluralName = KubePluralName)]
@@ -654,6 +653,7 @@ public partial class V1beta1ComputeInstanceGroupManager : IKubernetesObject<V1Ob
     [JsonPropertyName("metadata")]
     public V1ObjectMeta Metadata { get; set; }
 
+    /// <summary>ComputeInstanceGroupManagerSpec defines the desired state of ComputeInstanceGroupManager</summary>
     [JsonPropertyName("spec")]
     public required V1beta1ComputeInstanceGroupManagerSpec Spec { get; set; }
 
