713027c2cf290705b41d32e6020027f7f6f3f1c4
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2026-06-29 12:34:16 -0700

    feat: Update CRDs (#250)

---
+++ Generated/KubernetesCRDModelGen.SourceGenerator/KubernetesCRDModelGen.SourceGenerator.SourceGenerator/v1alpha1.dialogflowsiptrunks.dialogflow.cnrm.cloud.google.com.g.cs
@@ -0,0 +1,208 @@
+﻿#nullable enable
+using k8s;
+using k8s.Models;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+
+namespace KubernetesCRDModelGen.Models.dialogflow.cnrm.cloud.google.com;
+/// <summary>DialogflowSipTrunk is the Schema for the DialogflowSipTrunk API</summary>
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+[KubernetesEntity(Group = KubeGroup, Kind = KubeKind, ApiVersion = KubeApiVersion, PluralName = KubePluralName)]
+public partial class V1alpha1DialogflowSipTrunkList : IKubernetesObject<V1ListMeta>, IItems<V1alpha1DialogflowSipTrunk>
+{
+    public const string KubeApiVersion = "v1alpha1";
+    public const string KubeKind = "DialogflowSipTrunkList";
+    public const string KubeGroup = "dialogflow.cnrm.cloud.google.com";
+    public const string KubePluralName = "dialogflowsiptrunks";
+    /// <summary>APIVersion defines the versioned schema of this representation of an object. Servers should convert recognized schemas to the latest internal value, and may reject unrecognized values. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources</summary>
+    [JsonPropertyName("apiVersion")]
+    public string ApiVersion { get; set; } = "dialogflow.cnrm.cloud.google.com/v1alpha1";
+
+    /// <summary>Kind is a string value representing the REST resource this object represents. Servers may infer this from the endpoint the client submits requests to. Cannot be updated. In CamelCase. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds</summary>
+    [JsonPropertyName("kind")]
+    public string Kind { get; set; } = "DialogflowSipTrunkList";
+
+    /// <summary>ListMeta describes metadata that synthetic resources must have, including lists and various status objects. A resource may have only one of {ObjectMeta, ListMeta}.</summary>
+    [JsonPropertyName("metadata")]
+    public V1ListMeta? Metadata { get; set; }
+
+    /// <summary>List of V1alpha1DialogflowSipTrunk objects.</summary>
+    [JsonPropertyName("items")]
+    public required IList<V1alpha1DialogflowSipTrunk> Items { get; set; }
+}
+
+/// <summary>The project that this resource belongs to.</summary>
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+public partial class V1alpha1DialogflowSipTrunkSpecProjectRef
+{
+    /// <summary>The `projectID` field of a project, when not managed by Config Connector.</summary>
+    [JsonPropertyName("external")]
+    public string? External { get; set; }
+
+    /// <summary>The kind of the Project resource; optional but must be `Project` if provided.</summary>
+    [JsonPropertyName("kind")]
+    public string? Kind { get; set; }
+
+    /// <summary>The `name` field of a `Project` resource.</summary>
+    [JsonPropertyName("name")]
+    public string? Name { get; set; }
+
+    /// <summary>The `namespace` field of a `Project` resource.</summary>
+    [JsonPropertyName("namespace")]
+    public string? Namespace { get; set; }
+}
+
+/// <summary>DialogflowSipTrunkSpec defines the desired state of DialogflowSipTrunk</summary>
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+public partial class V1alpha1DialogflowSipTrunkSpec
+{
+    /// <summary>Optional. Human readable alias for this trunk.</summary>
+    [JsonPropertyName("displayName")]
+    public string? DisplayName { get; set; }
+
+    /// <summary>Required. The expected hostnames in the peer certificate from partner that is used for TLS authentication.</summary>
+    [JsonPropertyName("expectedHostname")]
+    public required IList<string> ExpectedHostname { get; set; }
+
+    /// <summary>Immutable. The location of this resource.</summary>
+    [JsonPropertyName("location")]
+    public required string Location { get; set; }
+
+    /// <summary>The project that this resource belongs to.</summary>
+    [JsonPropertyName("projectRef")]
+    public required V1alpha1DialogflowSipTrunkSpecProjectRef ProjectRef { get; set; }
+
+    /// <summary>The DialogflowSipTrunk name. If not given, the metadata.name will be used.</summary>
+    [JsonPropertyName("resourceID")]
+    public string? ResourceID { get; set; }
+}
+
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+public partial class V1alpha1DialogflowSipTrunkStatusConditions
+{
+    /// <summary>Last time the condition transitioned from one status to another.</summary>
+    [JsonPropertyName("lastTransitionTime")]
+    public string? LastTransitionTime { get; set; }
+
+    /// <summary>Human-readable message indicating details about last transition.</summary>
+    [JsonPropertyName("message")]
+    public string? Message { get; set; }
+
+    /// <summary>Unique, one-word, CamelCase reason for the condition&apos;s last transition.</summary>
+    [JsonPropertyName("reason")]
+    public string? Reason { get; set; }
+
+    /// <summary>Status is the status of the condition. Can be True, False, Unknown.</summary>
+    [JsonPropertyName("status")]
+    public string? Status { get; set; }
+
+    /// <summary>Type is the type of the condition.</summary>
+    [JsonPropertyName("type")]
+    public string? Type { get; set; }
+}
+
+/// <summary>Output only. The error details for the connection. Only populated when authentication errors occur.</summary>
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+public partial class V1alpha1DialogflowSipTrunkStatusObservedStateConnectionsErrorDetails
+{
+    /// <summary>Output only. The status of the certificate authentication.</summary>
+    [JsonPropertyName("certificateState")]
+    public string? CertificateState { get; set; }
+
+    /// <summary>The error message provided from SIP trunking auth service.</summary>
+    [JsonPropertyName("errorMessage")]
+    public string? ErrorMessage { get; set; }
+}
+
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+public partial class V1alpha1DialogflowSipTrunkStatusObservedStateConnections
+{
+    /// <summary>Output only. The unique identifier of the SIP Trunk connection.</summary>
+    [JsonPropertyName("connectionID")]
+    public string? ConnectionID { get; set; }
+
+    /// <summary>Output only. The error details for the connection. Only populated when authentication errors occur.</summary>
+    [JsonPropertyName("errorDetails")]
+    public V1alpha1DialogflowSipTrunkStatusObservedStateConnectionsErrorDetails? ErrorDetails { get; set; }
+
+    /// <summary>Output only. State of the connection.</summary>
+    [JsonPropertyName("state")]
+    public string? State { get; set; }
+
+    /// <summary>Output only. When the connection status changed.</summary>
+    [JsonPropertyName("updateTime")]
+    public string? UpdateTime { get; set; }
+}
+
+/// <summary>ObservedState is the state of the resource as most recently observed in GCP.</summary>
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+public partial class V1alpha1DialogflowSipTrunkStatusObservedState
+{
+    /// <summary>Output only. Connections of the SIP trunk.</summary>
+    [JsonPropertyName("connections")]
+    public IList<V1alpha1DialogflowSipTrunkStatusObservedStateConnections>? Connections { get; set; }
+}
+
+/// <summary>DialogflowSipTrunkStatus defines the config connector machine state of DialogflowSipTrunk</summary>
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+public partial class V1alpha1DialogflowSipTrunkStatus
+{
+    /// <summary>Conditions represent the latest available observations of the object&apos;s current state.</summary>
+    [JsonPropertyName("conditions")]
+    public IList<V1alpha1DialogflowSipTrunkStatusConditions>? Conditions { get; set; }
+
+    /// <summary>A unique specifier for the DialogflowSipTrunk resource in GCP.</summary>
+    [JsonPropertyName("externalRef")]
+    public string? ExternalRef { get; set; }
+
+    /// <summary>ObservedGeneration is the generation of the resource that was most recently observed by the Config Connector controller. If this is equal to metadata.generation, then that means that the current reported status reflects the most recent desired state of the resource.</summary>
+    [JsonPropertyName("observedGeneration")]
+    public long? ObservedGeneration { get; set; }
+
+    /// <summary>ObservedState is the state of the resource as most recently observed in GCP.</summary>
+    [JsonPropertyName("observedState")]
+    public V1alpha1DialogflowSipTrunkStatusObservedState? ObservedState { get; set; }
+}
+
+/// <summary>DialogflowSipTrunk is the Schema for the DialogflowSipTrunk API</summary>
+[global::System.CodeDom.Compiler.GeneratedCode("KubernetesCRDModelGen", "1.6.0+0fbafdb9fc339df17b265ba23ecc4a7be2359877")]
+[global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+[KubernetesEntity(Group = KubeGroup, Kind = KubeKind, ApiVersion = KubeApiVersion, PluralName = KubePluralName)]
+public partial class V1alpha1DialogflowSipTrunk : IKubernetesObject<V1ObjectMeta>, ISpec<V1alpha1DialogflowSipTrunkSpec>, IStatus<V1alpha1DialogflowSipTrunkStatus?>
+{
+    public const string KubeApiVersion = "v1alpha1";
+    public const string KubeKind = "DialogflowSipTrunk";
+    public const string KubeGroup = "dialogflow.cnrm.cloud.google.com";
+    public const string KubePluralName = "dialogflowsiptrunks";
+    /// <summary>APIVersion defines the versioned schema of this representation of an object. Servers should convert recognized schemas to the latest internal value, and may reject unrecognized values. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources</summary>
+    [JsonPropertyName("apiVersion")]
+    public string ApiVersion { get; set; } = "dialogflow.cnrm.cloud.google.com/v1alpha1";
+
+    /// <summary>Kind is a string value representing the REST resource this object represents. Servers may infer this from the endpoint the client submits requests to. Cannot be updated. In CamelCase. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds</summary>
+    [JsonPropertyName("kind")]
+    public string Kind { get; set; } = "DialogflowSipTrunk";
+
+    /// <summary>Standard object&apos;s metadata. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#metadata</summary>
+    [JsonPropertyName("metadata")]
+    public V1ObjectMeta Metadata { get; set; }
+
+    /// <summary>DialogflowSipTrunkSpec defines the desired state of DialogflowSipTrunk</summary>
+    [JsonPropertyName("spec")]
+    public required V1alpha1DialogflowSipTrunkSpec Spec { get; set; }
+
+    /// <summary>DialogflowSipTrunkStatus defines the config connector machine state of DialogflowSipTrunk</summary>
+    [JsonPropertyName("status")]
+    public V1alpha1DialogflowSipTrunkStatus? Status { get; set; }
+}
